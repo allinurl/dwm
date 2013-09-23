d92dfbe716ee2844370b0ed6a28a0a67f9099cb4
Gerardo Orellana <goaccess@prosoftcorp.com>
2013-09-23 06:55:29 -0500

    added personal keyboard shortcuts

--- config.h
+++ config.h
@@ -19,18 +19,18 @@ static const char colors[NUMCOLORS][ColLast][9] = {
 };
 
 static const unsigned int borderpx = 1; /* border pixel of windows   */
-static const unsigned int gappx = 7;    /* gap pixel between windows */
+static const unsigned int gappx = 5;    /* gap pixel between windows */
 static const unsigned int snap = 8;     /* snap pixel                */
 static const Bool showbar = True;       /* False means no bar        */
 static const Bool topbar = True;        /* False means bottom bar    */
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "prod", "dev", "term", "web", "media", "misc" };
 
 static const Rule rules[] = {
-   /* class      instance    title       tags mask     isfloating   monitor */
-   {"Gimp", NULL, NULL, 0, True, -1},
-   {"Firefox", NULL, NULL, 1 << 8, False, -1},
+   /*WM_CLASS     WM_CLASS WM_NAME
+      class        instance title          tags mask   isfloating  monitor */
+   {"Chromium", NULL, NULL, 1 << 3, False, -1},
 };
 
 /* layout(s) */
@@ -62,10 +62,12 @@ static const char *dmenucmd[] =
    "-sb", colors[1][ColBG], "-sf", colors[1][ColFG], NULL
 };
 static const char *termcmd[] = { "uxterm", NULL };
+static const char *browser[] = { "chromium", NULL };
 
 static Key keys[] = {
    /* modifier                     key        function        argument */
    {MODKEY, XK_p, spawn, {.v = dmenucmd}},
+   {MODKEY, XK_g, spawn, {.v = browser}},
    {MODKEY | ShiftMask, XK_Return, spawn, {.v = termcmd}},
    {MODKEY, XK_b, togglebar, {0}},
    {MODKEY, XK_j, focusstack, {.i = +1}},
@@ -74,7 +76,7 @@ static Key keys[] = {
    {MODKEY, XK_d, incnmaster, {.i = -1}},
    {MODKEY, XK_h, setmfact, {.f = -0.05}},
    {MODKEY, XK_l, setmfact, {.f = +0.05}},
-   {MODKEY, XK_Return, zoom, {0}},
+   {MODKEY, XK_Return, spawn, {.v = termcmd}},
    {MODKEY, XK_Tab, view, {0}},
    {MODKEY | ShiftMask, XK_c, killclient, {0}},
    {MODKEY, XK_t, setlayout, {.v = &layouts[0]}},
@@ -96,7 +98,7 @@ static Key keys[] = {
       TAGKEYS (XK_6, 5)
       TAGKEYS (XK_7, 6)
       TAGKEYS (XK_8, 7)
-   TAGKEYS (XK_9, 8) {MODKEY | ShiftMask, XK_q, quit, {0}},
+   TAGKEYS (XK_9, 8) {MODKEY | ControlMask, XK_q, quit, {0}},
 };
 
 /* button definitions */
