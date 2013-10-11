7c3cfa42332fdb732b3ba1aa84cfb2a7d2fc8304
Gerardo Orellana <goaccess@prosoftcorp.com>
2013-10-11 13:16:45 -0500

    added escape binding to xscreensaver -lock

--- config.h
+++ config.h
@@ -68,6 +68,7 @@ static Key keys[] = {
    /* modifier                     key        function        argument */
    {MODKEY, XK_p, spawn, {.v = dmenucmd}},
    {MODKEY, XK_g, spawn, {.v = browser}},
+   {MODKEY, XK_Escape, spawn, SHCMD ("xscreensaver-command -lock")},
    {MODKEY | ShiftMask, XK_Return, spawn, {.v = termcmd}},
    {MODKEY, XK_b, togglebar, {0}},
    {MODKEY, XK_Right, focusstack, {.i = +1}},
