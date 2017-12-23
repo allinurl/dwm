277ff9d3fbe222a1cb816b6267c8af5c4ea9444c
Gerardo Orellana <goaccess@prosoftcorp.com>
2017-12-23 13:42:55 -0600

    Updated config.h format.

--- config.h
+++ config.h
@@ -129,7 +129,7 @@ static Key keys[] = {
 /* button definitions */
 /* click can be ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin */
 static Button buttons[] = {
-	/* click                event mask      button          function        argument */
+	/* click         eventmask button    function        argument */
 	{ ClkLtSymbol   , 0      , Button1 , setlayout      , {0} }                , 
 	{ ClkLtSymbol   , 0      , Button3 , setlayout      , {.v = &layouts[2]} } , 
 	{ ClkWinTitle   , 0      , Button2 , zoom           , {0} }                , 
