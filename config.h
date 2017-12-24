9bffe0be389df22396618c445e986918d82435ee
Gerardo Orellana <goaccess@prosoftcorp.com>
2017-12-24 13:44:12 -0600

    Updated config.h format.

--- config.h
+++ config.h
@@ -129,17 +129,17 @@ static Key keys[] = {
 /* button definitions */
 /* click can be ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin */
 static Button buttons[] = {
-	/* click         eventmask button    function        argument */
-	{ ClkLtSymbol   , 0      , Button1 , setlayout      , {0} }, 
-	{ ClkLtSymbol   , 0      , Button3 , setlayout      , {.v = &layouts[2]} }, 
-	{ ClkWinTitle   , 0      , Button2 , zoom           , {0} }, 
-	{ ClkStatusText , 0      , Button2 , spawn          , {.v = termcmd } }, 
-	{ ClkClientWin  , MODKEY , Button1 , movemouse      , {0} }, 
-	{ ClkClientWin  , MODKEY , Button2 , togglefloating , {0} }, 
-	{ ClkClientWin  , MODKEY , Button3 , resizemouse    , {0} }, 
-	{ ClkTagBar     , 0      , Button1 , view           , {0} }, 
-	{ ClkTagBar     , 0      , Button3 , toggleview     , {0} }, 
-	{ ClkTagBar     , MODKEY , Button1 , tag            , {0} }, 
-	{ ClkTagBar     , MODKEY , Button3 , toggletag      , {0} }, 
+  /* click         eventmask button    function        argument */
+  { ClkLtSymbol   , 0      , Button1 , setlayout      , {0} }, 
+  { ClkLtSymbol   , 0      , Button3 , setlayout      , {.v = &layouts[2]} }, 
+  { ClkWinTitle   , 0      , Button2 , zoom           , {0} }, 
+  { ClkStatusText , 0      , Button2 , spawn          , {.v = termcmd } }, 
+  { ClkClientWin  , MODKEY , Button1 , movemouse      , {0} }, 
+  { ClkClientWin  , MODKEY , Button2 , togglefloating , {0} }, 
+  { ClkClientWin  , MODKEY , Button3 , resizemouse    , {0} }, 
+  { ClkTagBar     , 0      , Button1 , view           , {0} }, 
+  { ClkTagBar     , 0      , Button3 , toggleview     , {0} }, 
+  { ClkTagBar     , MODKEY , Button1 , tag            , {0} }, 
+  { ClkTagBar     , MODKEY , Button3 , toggletag      , {0} }, 
 };
 
