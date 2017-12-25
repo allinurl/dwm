50b74916bc981b9dd62d80bbb7c395bc0cc580be
Gerardo Orellana <goaccess@prosoftcorp.com>
2017-12-25 11:18:58 -0600

    Updated config.h format.

--- config.h
+++ config.h
@@ -113,8 +113,8 @@ static Key keys[] = {
   {MODKEY               , XK_period  , focusmon       , {.i = +1}},
   {MODKEY | ShiftMask   , XK_comma   , tagmon         , {.i = -1}},
   {MODKEY | ShiftMask   , XK_period  , tagmon         , {.i = +1}},
-  //{MODKEY | ControlMask , XK_j                     , pushdown       , {0}},
-  //{MODKEY | ControlMask , XK_k                     , pushup         , {0}},
+  //{MODKEY | ControlMask , XK_j     , pushdown       , {0}},
+  //{MODKEY | ControlMask , XK_k     , pushup         , {0}},
   TAGKEYS (XK_1, 0)
   TAGKEYS (XK_2, 1)
   TAGKEYS (XK_3, 2)
